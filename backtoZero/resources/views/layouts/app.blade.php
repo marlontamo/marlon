2cd2108cfbd3a47bbda5f4c6ec6fadae7c891c3b
marlontamo <marlontamo@gmail.com>
2022-10-21 11:51:47 +0800

    "fixing UI for login and register"

--- backtoZero/resources/views/layouts/app.blade.php
+++ backtoZero/resources/views/layouts/app.blade.php
@@ -9,9 +9,10 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="{{asset('js/app.js')}}"></script>
+        
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -29,5 +30,6 @@
                 {{ $slot }}
             </main>
         </div>
+        <footer>footer</footer>
     </body>
 </html>
